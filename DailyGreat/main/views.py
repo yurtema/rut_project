e9391353d7c544b2032a137077076bcf166d59dd
Michal460 <147305790+Michal460@users.noreply.github.com>
2023-10-24 11:01:33 +0300

    Update views.py

--- DailyGreat/main/views.py
+++ DailyGreat/main/views.py
@@ -8,3 +8,18 @@ def about(request):
 
 def child(request):
     return render(request, 'main/child.html')
+
+def cinema(request):
+    return render(request, 'main/cinema.html')
+
+def theatre(request):
+    return render(request, 'main/theatre.html')
+
+def exhibition(request):
+    return render(request, 'main/exhibition.html')
+
+def concert(request):
+    return render(request, 'main/concert.html')
+
+def other(request):
+    return render(request, 'main/other.html')
