3cd56f88013ab7962dac256bf10aaa8ce0495cb9
Michal460 <147305790+Michal460@users.noreply.github.com>
2023-10-30 05:56:25 +1000

    Update views.py

--- DailyGreat/main/views.py
+++ DailyGreat/main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'main/index2.html')
+    return render(request, 'main/index.html')
 
 def about(request):
     return render(request, 'main/about.html')
