aeaf01c3495f2013c5af26b07a61a84b3be31f15
Michal460 <147305790+Michal460@users.noreply.github.com>
2023-11-27 10:02:37 +0300

    Update urls.py

--- DailyGreat/main/urls.py
+++ DailyGreat/main/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('exhibition', views.exhibition, name = 'exhibition'),
     path('concert', views.concert, name = 'concert'),
     path('other', views.other, name = 'other'),
+    path('calendary', views.calendar, name = 'calendary'),
 ]
