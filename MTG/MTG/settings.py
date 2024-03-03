e3e7f65a2a63dc5b6c494d029d83b4e717adc862
Michal460 <147305790+Michal460@users.noreply.github.com>
2024-03-03 16:35:55 +0300

    Вернул к прежнему настройки

--- MTG/MTG/settings.py
+++ MTG/MTG/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'MTG.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'main', 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
