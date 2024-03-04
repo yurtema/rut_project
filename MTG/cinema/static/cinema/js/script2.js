bb3c32566ad440f3c0564bebabe6eee8869f3d9d
Michal460 <147305790+Michal460@users.noreply.github.com>
2024-03-04 22:59:55 +0300

    Update script2.js

--- MTG/cinema/static/cinema/js/script2.js
+++ MTG/cinema/static/cinema/js/script2.js
@@ -1,1 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+const block4 = document.getElementById('block4');
 
+for (let i = 0; i < 4; i++) {
+const block5 = document.createElement('div');
+block5.classList.add('block5');
+block4.appendChild(block5);
+}
+});
