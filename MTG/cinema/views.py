cc8f04a5f4d80889ff0cd87fb5e9eaeb7a417556
Michal460 <147305790+Michal460@users.noreply.github.com>
2024-03-11 19:20:45 +0300

    Update views.py

--- MTG/cinema/views.py
+++ MTG/cinema/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from .models import Cinema_events
 
 def events_catalog1(request):
-    events = Cinema_events.objects.exclude(title__isnull=True).exclude(title__exact='None inf')
-    unique_events = list(set(events))
-    return render(request, "cinema/index.html", {"events": unique_events})
+    events = Cinema_events.objects.all()
+    for event in events:
+        if event.images:
+            event.first_images = event.images[0]
+    return render(request, "cinema/index.html", {"events": events})
 
 def event_detail1(request, event_id):
     event = Cinema_events.objects.get(id=event_id)
