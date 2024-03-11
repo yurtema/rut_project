2938e11123c4e06e03d01fe61d217146d9ffdf69
Michal460 <147305790+Michal460@users.noreply.github.com>
2024-03-11 19:42:28 +0300

    Update views.py

--- MTG/cinema/views.py
+++ MTG/cinema/views.py
@@ -5,9 +5,12 @@ def events_catalog1(request):
     events = Cinema_events.objects.all()
     for event in events:
         if event.images:
-            event.first_images = event.images[0]
+            event.urls_list = event.images[1:-1]
+            event.first_images = event.urls_list.strip("[]").split(", ")[0][1:-1]
     return render(request, "cinema/index.html", {"events": events})
 
 def event_detail1(request, event_id):
     event = Cinema_events.objects.get(id=event_id)
+    event.urls_list = event.images[1:-1]
+    event.first_image = event.urls_list.strip("[]").split(", ")[0][1:-1]
     return render(request, 'cinema/event_details.html', {'event': event})
