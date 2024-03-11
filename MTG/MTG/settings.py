c2b32b75a1cbf5a5b73214626f8424f452b546f8
Michal460 <147305790+Michal460@users.noreply.github.com>
2024-03-11 19:23:24 +0300

    Update settings.py

--- MTG/MTG/settings.py
+++ MTG/MTG/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'MTG.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db1.sqlite3',
     }
 }
 
